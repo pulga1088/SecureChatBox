0a84b34639b39b32f03e8e64e06e4e770b8fb5d1
pulga1088 <adityamurali71@gmail.com>
2026-07-01 18:35:29 +0530

    Upgrade project to Expo SDK 56 to support newer Expo Go app versions on other devices

--- frontend/src/screens/ChatScreen.tsx
+++ frontend/src/screens/ChatScreen.tsx
@@ -1270,7 +1270,11 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   },
   uploadingOverlay: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     backgroundColor: 'rgba(0, 0, 0, 0.65)',
     justifyContent: 'center',
     alignItems: 'center',
